966de876091c55dbf412480d8ede1da93e7d299b
amyfruge <amy.fruge.16@student.prcc.edu>
2024-02-29 23:27:23 -0600

    class project

--- clientSide/Chapter03/chapter/js03.js
+++ clientSide/Chapter03/chapter/js03.js
@@ -4,12 +4,56 @@
 
      Tipton Turbines
      Program to display games results in a web table
-     Author: 
-     Date:   
+     Author: Amy Fruge
+     Date: 02/29/2024
 
      Filename: js03.js
  */
 
 //Days of the week
-let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday",
-                 "Thursday", "Friday", "Saturday"]
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+                "Thursday", "Friday", "Saturday"];
+
+//Function to write weekday names into the calendar
+function addWeekDays() {
+    let i = 0; //initial counter value
+
+    //reference the collection of heading cells
+    let headingCells = document.getElementsByTagName("th");
+
+    //write each of the seven days into a heading cell
+    while (i < 7) {
+        headingCells[i].innerHTML = weekDays[i];
+
+        //increase the counter by 1
+        i++;
+    }
+    
+}
+
+//Function to write game information into the calendar
+function showGames() {
+    for (let i = 0; i < gameDates.length; i++) {
+        let gameInfo = "";
+
+        //open the paragraph
+        gameInfo += "<p>";
+        
+        //include the opponent
+        gameInfo += gameOpponents[i] + "<br>";
+
+        //include the result and score
+        gameInfo += gameResults[i] + ": (" + runsScored[i] +
+        "-" + runsAllowed[i] + ")";
+
+        //close the paragraph
+        gameInfo += "</p>";
+
+        //write the information into a table cell
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo)
+    }
+}
+
+window.addEventListener("load", addWeekDays);
+window.addEventListener("load", showGames);
